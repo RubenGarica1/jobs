c606c3a57e7c2a2ffdd59638e7dfadeda174d037
Ruben A Garcia Acosta <ruben@MacBook-Pro-de-Ruben.local>
2019-07-26 00:06:30 -0600

    [MOD] cambios chilos

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-var CronJob = require('cron').CronJob;
+
+
+ var CronJob = require('cron').CronJob;
+ const datos = [{name: "ruben", date:"0 15 8 26 07 *"}, {name:'ada', date :"0 30 8 26 07 *"} ]
+ datos.forEach(res =>{
+     new CronJob(res.date, function() {
+        console.log(`--------------------------${res.name}-----------------------------------`)
+        console.log(Date())
+        console.log(`hola ${res.name} que onda son las ${res.date}`);
+      }, null, true, 'America/Los_Angeles');
+})
+
+
 new CronJob('0 0 8 26 07 *', function() {
   console.log('You will see this message every second');
 }, null, true, 'America/Los_Angeles');
